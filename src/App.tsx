c230f44bec0970a1aac281394cdd4f5526dc753d
timokie <timokie@126.com>
2020-09-16 11:07:42 +0800

    feat: button基本样式

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button disabled classname="customer"> Hello </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large} onClick={()=> {alert('fighting!')}}> Hello </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Baidu Link </Button>
+        <Button> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
       </header>
     </div>
   );
