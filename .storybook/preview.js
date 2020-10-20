7bfbd24910cbbfc676ae3da119050904d0b2ff52
timokie <timokie@126.com>
2020-10-20 16:15:40 +0800

    feat: 完善文档

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,21 @@
 import "../src/styles/index.scss"
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import "./fix_info_style.scss"
+import React from 'react'
+
 addDecorator(withInfo); 
+const wrapperStyle = {
+  padding: '20px 40px',
+}
+const storyWrapper = (stroyFn) => (
+  <div style={wrapperStyle}>
+    <h3>组件演示</h3>
+    {stroyFn()}
+  </div>
+)
+addDecorator(storyWrapper)
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  info: {inline: true}
+  info: {inline: true, header: false}
 }
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,22 +1,17 @@
 import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
 import classnames from 'classnames';
-// export enum ButtonType {
-//   Primary = 'primary',
-//   Default = 'default',
-//   Danger = 'danger',
-//   Link = 'link'
-// }
-// export enum ButtonSize {
-//   Large = 'lg',
-//   Small = 'sm'
-// }
 export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 export type ButtonSize = 'lg' | 'sm'
+
 interface BaseButtonProps {
   classname?: string;
+  /**类型 */
   btnType?: ButtonType;
+  /**尺寸 */
   size?: ButtonSize;
+  /**a链接的href */
   href?: string;
+  /** 是否禁用 */
   disabled?: boolean;
   children?: React.ReactNode;
 }
@@ -24,7 +19,15 @@ type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
 type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
 
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
-
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互，支持 HTML button 和 a 链接 的所有属性
+ * 
+ * ### 引用方法
+ * 
+ * ~~~js
+ * import { Button } from 'beryl'
+ * ~~~
+ */
 export const Button: FC<ButtonProps> = (props) => {
   const {
     classname,
